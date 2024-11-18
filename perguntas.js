88a674df265fab55571615f15a1ee653aa3162ec
OrionCHB <142427494+OrionCHB@users.noreply.github.com>
2024-11-18 11:47:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Naruto',
+    'Quem foi o primeiro Hokage?',
+    'O primeiro Hokage foi  Hashirama Senju'
+)
+
+
+criaCartao(
+    'Naruto',
+    'O que é chakra?',
+    'Chakra é a energia formada a partir da junção da energia espiritual e física, usado pelos ninjas para fazer tanto ninjutsu quanto genjutsu'
+)
+
+
+criaCartao(
+    'Naruto',
+    'O que é uma Kekkei Genkai?',
+    'São habilidades passadas geneticamente dentro de clãs específicos, ou que eventualmente são manifestadas por indivíduos devido às suas próprias características.'
+)
+
+
+criaCartao(
+    'Naruto',
+    'Quais são os nomes ddas Bijuus?',
+    'Ichibi (Shukaku) Nibi (Matatabi) Sanbi (Isobu) Yonbi (Son Goku) Gobi (Kokuou) Rokubi (Saiken) Nanabi/Shichibi (Choumei) Hachibi (Gyuki) Kyūbi (Kurama) '
+)
+
+
+criaCartao(
+    'Naruto',
+    'Quem é o pai de Hashirama Senju?',
+    'Butsuma, líder do clã Senju'
+)
+
+
+criaCartao(
+    'Naruto',
+    'Quais são os 5 elementos de chakra em Naruto?',
+    'Estilo Fogo, Estilo Vento, Estilo Raio, Estilo Terra, Estilo Água '
+)
+
+
+criaCartao(
+    'Naruto',
+    'Quem são os membros da Akatsuki?',
+    'Nagato, Konan, Itachi, Tobi, Zetsu, Deidara, Sasori, Orochimaru, Hidan, Kakuzu e Kisame'
+)
+
+
+criaCartao(
+    'Naruto',
+    'Quem criou Naruto?',
+    'Masashi Kishimoto'
+)
